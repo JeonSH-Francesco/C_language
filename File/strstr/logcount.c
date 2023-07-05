703446b1bc5ee395777f662deed57045c3da4d51
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-07-05 19:08:07 +0900

    Update logcount.c

--- File/strstr/logcount.c
+++ File/strstr/logcount.c
@@ -1,4 +1,38 @@
-#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char buf[100];
+	char ans[10] = "SUCCESS";
+
+	int cnt = 0;
+	FILE* fp = fopen("log.txt", "r");
+
+	if (fp == NULL) {
+		printf("File open error!");
+		return -1;
+	}
+	//while(!feof(fp))구문을 사용하면 파일의 끝에 도달하기 전 까지는 
+	//0이 아닌 값을 반환 하므로,
+	//파일의 끝에 도달한 이후에도 한 번 더 루프를 실행하게 됩니다.
+	//이로 인해 cnt 변수가 예상보다 큰 값을 출력합니다.
+	//따라서 이를 해결하기 위해 fgets 함수를 이용하여 
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		if (strstr(buf, ans) != NULL) {
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	fclose(fp);
+
+	return 0;
+}
+
+
+
+/*
 #include <stdio.h>
 #include <string.h>
 
@@ -16,10 +50,10 @@ int main() {
 
 	while (!feof(fp)) {
 		fgets(buf, sizeof(buf), fp);
-		printf("%s", buf);
+		//printf("%s", buf);
 		
 		if (strstr(buf, ans) != NULL) {
-			printf("%s", buf);
+			//printf("%s", buf);
 			cnt++;
 		}
 	}
@@ -29,3 +63,4 @@ int main() {
 
 	return 0;
 }
+*/
