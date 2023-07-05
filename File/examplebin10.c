4ab32b01fa8b0bb2a0e2ac25edd0d6197a720613
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-07-05 11:34:27 +0900

    Update examplebin10.c

--- File/examplebin10.c
+++ File/examplebin10.c
@@ -2,7 +2,32 @@
 #include <stdio.h>
 
 int main() {
-	unsigned char buf[11]; // 버퍼를 unsigned char 배열로 선언하여 16진수로 출력합니다.
+	unsigned char buf[11];
+	FILE* fp = fopen("example.bin", "rb");
+
+	if (fp == NULL) {
+		printf("Error : File open failed");
+		return -1;
+	}
+	fread(buf, sizeof(unsigned char), 10, fp);
+
+	for (int i = 0; i < 10; i++) {
+		//printf("[%d] : %02x", i, buf[i]);
+		printf("%02x ",buf[i]);
+	}
+	printf("\n");
+
+	fclose(fp);
+
+	return 0;
+}
+------
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	unsigned char buf[10]; // 버퍼를 unsigned char 배열로 선언하여 16진수로 출력합니다.
 	FILE* fp = fopen("example.bin", "rb");
 
 	if (fp == NULL) {
@@ -10,13 +35,15 @@ int main() {
 		return -1;
 	}
 
-	fread(buf, sizeof(unsigned char), 10, fp);
-
-	for (int i = 0; i < 10; i++) {
-		printf("[%d] %02x ", i, buf[i]); // 16진수로 출력하기 위해 %0x %02x가 있는데 두 자리로 출력하기 위해 %02x로 함.
+	while (fread(buf, sizeof(unsigned char), 10, fp) > 0) {
+		for (int i = 0; i < 10; i++) {
+			printf("%02X ", buf[i]); // 16진수로 출력하기 위해 %02X를 사용합니다.
+		}
+		putchar('\n');
 	}
 
 	fclose(fp);
 
 	return 0;
 }
+*/
