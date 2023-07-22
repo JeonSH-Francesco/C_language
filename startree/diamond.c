52c0aa9e12a341721f9daf30bf0365039bafa055
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-07-22 16:54:33 +0900

    Create diamond.c

---
+++ startree/diamond.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+  int num=0;
+  printf("Input number: ");
+  scanf("%d",&num);
+  
+  for(int i=0;i<num;i++){
+    for(int j=num-1;j>i;j--){
+        printf(" ");
+    }
+	    
+    for(int j=0;j<2*i+1;j++){
+	    printf("*");
+    }
+    printf("\n");
+  }
+
+  for(int i=0;i<num;i++){
+    for(int j=0;j<i;j++){
+         printf(" ");
+     }
+
+    for(int j=2*num-1;j>2*i;j--){
+	printf("*");
+    }
+    printf("\n");
+}
+
+}
