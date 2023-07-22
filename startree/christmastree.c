f05dded9b2b0481b1a0f0a590d47b5956511c7b7
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-07-22 16:52:26 +0900

    Create christmastree.c

---
+++ startree/christmastree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int num=0;
+  printf("Input number: ");
+  scanf("%d",&num):
+  
+  for(int i=0;i<num;i++){
+    for(int j=num-1;j>i;j--){
+        printf(" ");
+    }
+	    
+    for(int j=0;j<2*i+1;j++){
+	    printf("*");
+    }
+    printf("\n");
+  }
+}
