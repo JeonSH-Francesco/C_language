d55263c81f3c1fb45959d1a5ec5a82a70fe4b1d9
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-10-23 13:14:09 +0900

    Create asc_star.c

---
+++ startree/asc_star.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int i = 0;
+	int j = 0;
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (j <= i) {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}
+/*
+result:
+*
+**
+***
+****
+*****
+
+*/
