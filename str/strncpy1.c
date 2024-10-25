919427ef1c9a2fc7d33bc44aeaa1bcad7eca876d
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-10-25 15:39:39 +0900

    Update strncpy1.c

--- str/strncpy1.c
+++ str/strncpy1.c
@@ -1,17 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_STR_LEN 80
 
-int main(){
-    char *src="abcdefg1234567";
-    char *dest=(char*)malloc(sizeof(char)*MAX_STR_LEN);
+int main() {
+    char* src = "abcdefg1234567";
+    char* dest = (char*)malloc(sizeof(char) * MAX_STR_LEN);
 
-    strncpy(dest,src,7);
+    strncpy(dest, src, 7);
+    dest[7] = '\0';
 
-    printf("src: %s\n",src);
-    printf("dest: %s\n",dest);
+    printf("src: %s\n", src);
+    printf("dest: %s\n", dest);
 
     free(dest);
     return 0;
