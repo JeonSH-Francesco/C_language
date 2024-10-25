eb34c98352279e324eb3cd32077feabc20ceb0be
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-10-25 15:30:54 +0900

    Update strlen1.c

--- str/strlen1.c
+++ str/strlen1.c
@@ -1,27 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define MAX_STR_LEN 80
 
-int main(){
-    char *src= "abcdefg1234567";
-    char *dest=(char*)malloc(sizeof(char)*MAX_STR_LEN);
-    char *key="123";
-    char *pos;
+int main() {
+    char* src = "abcdefg1234567";
+    char* dest=(char*)malloc(sizeof(char)*MAX_STR_LEN);
+    char* key = "123";
+    char* pos;
+    
+    pos=strstr(src, key);
 
-    pos=strstr(src,key);
-    printf("src: %s, index : %ld\n",src,pos-src);
+    printf("src: %s, index : %d\n",src,pos-src);
 
-    strncpy(dest,src,pos-src);
+    strncpy(dest, src, pos - src);
+    dest[pos - src] = '\0';
+    strncat(dest, pos + strlen(key), strlen(pos + strlen(key)));
+    printf("pos+strlen(key): %s\n", pos + strlen(key));
+    printf("dest : %s\n",dest);
 
-    strncat(dest, pos+strlen(key), strlen(pos-strlen(key)));
-
-    printf("pos+strlen(key): %s\n",pos+strlen(key));
-    printf("strlen(pos-strlen(key)): %d\n",strlen(pos-strlen(key)));
-    printf("dest: %s",dest);
 
     free(dest);
 
     return 0;
 }
+
